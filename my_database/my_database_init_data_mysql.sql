8fe643735a5e5181ec590fe7d12486ec2750a84c
astmain <1311192345@qq.com>
2025-12-03 22:36:39 +0800

    1

---
+++ my_database/my_database_init_data_mysql.sql
@@ -0,0 +1,27 @@
+-- ============================== my_db1 ==============================
+CREATE DATABASE IF NOT EXISTS my_db1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE my_db1;
+CREATE TABLE IF NOT EXISTS test_demo1 (
+    id INT AUTO_INCREMENT PRIMARY KEY                        COMMENT '自增主键',
+    name VARCHAR(100) NOT NULL                               COMMENT '姓名',
+    phone VARCHAR(20) NOT NULL UNIQUE                        COMMENT '手机号(不能为空且唯一)',
+    age INT                                                  COMMENT '年龄(整数)',
+    password VARCHAR(255) NOT NULL                           COMMENT '密码(加密存储)',
+    gender ENUM('MALE', 'FEMALE', 'OTHER') DEFAULT 'OTHER'   COMMENT '性别(MALE=男性的,FEMALE=女性的,OTHER=其他)'
+) engine=innodb COMMENT='测试_样板1表';
+
+
+
+
+
+-- ============================== my_db2 ==============================
+CREATE DATABASE IF NOT EXISTS my_db2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE my_db2;
+CREATE TABLE IF NOT EXISTS test_demo1 (
+    id INT AUTO_INCREMENT PRIMARY KEY                        COMMENT '自增主键',
+    name VARCHAR(100) NOT NULL                               COMMENT '姓名',
+    phone VARCHAR(20) NOT NULL UNIQUE                        COMMENT '手机号(不能为空且唯一)',
+    age INT                                                  COMMENT '年龄(整数)',
+    password VARCHAR(255) NOT NULL                           COMMENT '密码(加密存储)',
+    gender ENUM('MALE', 'FEMALE', 'OTHER') DEFAULT 'OTHER'   COMMENT '性别(MALE=男性的,FEMALE=女性的,OTHER=其他)'
+) engine=innodb COMMENT='测试_样板1表';
---
+++ sql_init.sql
@@ -0,0 +1,29 @@
+-- ============================== my_db1 ==============================
+CREATE DATABASE IF NOT EXISTS my_db1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE my_db1;
+
+CREATE TABLE IF NOT EXISTS test_demo1 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20),
+    age INT,
+    password VARCHAR(255) NOT NULL,
+    gender ENUM('MALE', 'Female', 'Other') DEFAULT 'Other'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+
+-- ============================== my_db2 ==============================
+CREATE DATABASE IF NOT EXISTS my_db2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE my_db2;
+CREATE TABLE IF NOT EXISTS test_demo1 (
+    id INT AUTO_INCREMENT PRIMARY KEY                        COMMENT '自增主键',
+    name VARCHAR(100) NOT NULL                               COMMENT '姓名',
+    phone VARCHAR(20) NOT NULL UNIQUE                        COMMENT '手机号(不能为空且唯一)',
+    age INT                                                  COMMENT '年龄(整数)',
+    password VARCHAR(255) NOT NULL                           COMMENT '密码(加密存储)',
+    gender ENUM('MALE', 'FEMALE', 'OTHER') DEFAULT 'OTHER'   COMMENT '性别(MALE=男性的,FEMALE=女性的,OTHER=其他)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
