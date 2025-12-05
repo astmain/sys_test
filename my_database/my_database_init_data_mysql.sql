44c7b1e47c4fb71cae2acb066e73a03feb3a7932
astmain <1311192345@qq.com>
2025-12-06 02:22:24 +0800

    1

--- my_database/my_database_init_data_mysql.sql
+++ my_database/my_database_init_data_mysql.sql
@@ -1,8 +1,9 @@
 -- ============================== my_db1 ==============================
 CREATE DATABASE IF NOT EXISTS my_db1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE my_db1;
+
 
 -- ============================== 测试_样板1表 ==============================
+USE my_db1;
 CREATE TABLE IF NOT EXISTS test_demo1 (
     id INT AUTO_INCREMENT PRIMARY KEY                        COMMENT '自增主键',
     name VARCHAR(100) NOT NULL                               COMMENT '姓名',
@@ -13,6 +14,7 @@ CREATE TABLE IF NOT EXISTS test_demo1 (
 ) engine=innodb COMMENT='测试_样板1表';
 
 -- ============================== 测试_学生表1 ==============================
+USE my_db1;
 CREATE TABLE IF NOT EXISTS test_student1 (
     name VARCHAR(100) NOT NULL                               COMMENT '姓名',
     age INT                                                  COMMENT '年龄(整数)',
@@ -25,6 +27,7 @@ INSERT INTO `my_db1`.`test_student1` (`id`, `name`, `age`, `sex`, `city`) VALUES
 INSERT INTO `my_db1`.`test_student1` (`id`, `name`, `age`, `sex`, `city`) VALUES (2, '钱二', 1, 'OTHER', '厦门');
 
 -- ============================== 测试_职位表1 ==============================
+USE my_db1;
 CREATE TABLE IF NOT EXISTS test_position1 (
     user_id INT                                              COMMENT '用户id',
     postition_name VARCHAR(100)                              COMMENT '职位名称',
@@ -34,8 +37,8 @@ INSERT INTO `my_db1`.`test_position1` (`user_id`, `postition_name`, `id`) VALUES
 INSERT INTO `my_db1`.`test_position1` (`user_id`, `postition_name`, `id`) VALUES (2, '体育委员', 2);
 
 
-
-
+-- ============================== 测试_上传文件1 ==============================
+USE my_db1;
 CREATE TABLE IF NOT EXISTS test_upload_file (
     file_name VARCHAR(100)                                    COMMENT '文件名',
     file_path VARCHAR(100)                                    COMMENT '文件路径',
@@ -47,8 +50,8 @@ CREATE TABLE IF NOT EXISTS test_upload_file (
 
 -- ============================== my_db2 ==============================
 CREATE DATABASE IF NOT EXISTS my_db2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE my_db2;
 -- ============================== 测试_样板1表 ==============================
+USE my_db2;
 CREATE TABLE IF NOT EXISTS test_demo1 (
     id INT AUTO_INCREMENT PRIMARY KEY                        COMMENT '自增主键',
     name VARCHAR(100) NOT NULL                               COMMENT '姓名',
@@ -60,6 +63,39 @@ CREATE TABLE IF NOT EXISTS test_demo1 (
 
 
 
--- ============================== my_db2 ==============================
-CREATE DATABASE IF NOT EXISTS my_db2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE my_db2;
+-- ============================== my_db3 ==============================
+CREATE DATABASE IF NOT EXISTS my_db3 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+
+-- ============================== 测试_样板1表 ==============================
+USE my_db3;
+drop table if exists test_user;
+CREATE TABLE IF NOT EXISTS test_user (
+    id INT AUTO_INCREMENT PRIMARY KEY                        COMMENT '自增主键',
+
+    phone VARCHAR(20) NOT NULL UNIQUE                        COMMENT '手机号(不能为空且唯一)',
+	    name VARCHAR(100) NOT NULL                               COMMENT '姓名',
+    age INT                                                  COMMENT '年龄(整数)',
+    password VARCHAR(255) NOT NULL                           COMMENT '密码(加密存储)',
+    avatar VARCHAR(255)                                      COMMENT '头像',
+    gender ENUM('MALE', 'FEMALE', 'OTHER') DEFAULT 'OTHER'   COMMENT '性别(MALE=男性的,FEMALE=女性的,OTHER=其他)'
+) engine=innodb COMMENT='测试_样板1表';
+
+
+
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (1, '1','姓名1',  0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (2, '2', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (3, '3', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (4, '4','姓名1',  0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (5, '5', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (6, '6', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (7, '7','姓名1',  0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (8, '8', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (9, '9', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (10, '10','姓名1',  0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (11, '11', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (12, '12', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (13, '13','姓名1',  0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (14, '14', '姓名', 0, '111', '111', 'MALE');
+INSERT INTO `my_db3`.`test_user` (`id`, `phone`, `name`, `age`, `password`, `avatar`, `gender`) VALUES (15, '15', '姓名', 0, '111', '111', 'MALE');
