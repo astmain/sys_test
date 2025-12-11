5f7b5b1d99f88d4ddd41344c7915de7471ccc316
astmain <1311192345@qq.com>
2025-12-11 17:08:33 +0800

    1

---
+++ 代码片段_my_code.js
@@ -0,0 +1,21 @@
+{
+	"con1": {
+		"scope": "javascript,typescript",
+		"prefix": "con1",
+		"body": [
+			"console.log(`111---:`,111)"
+		]
+
+	},
+	"sle1": {
+		"scope": "javascript,typescript",
+		"prefix": "sle1",
+		"body": [
+			"await new Promise((resolve) => setTimeout(resolve, 999999 * 1000))",
+			"$2"
+		]
+
+	}
+
+// C:\Users\admin\AppData\Roaming\Cursor\User\snippets\my_code.code-snippets
+}
